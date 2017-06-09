6b632091c0e6b8daee74e327e2b9dfd5fcb53884
Roshan Kandel <rkandel@healthgrades.com>
2017-06-09 15:48:06 -0600

    added files

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname+'/public',
+        filename:'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test:/\.js$/,
+                loader:'babel-loader'  //Run babel-loader in every .js file this transform non standard js like jsx es2015
+            }
+        ]
+    }
+}
