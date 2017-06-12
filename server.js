bd46643f589279870ba9fc974f44d36ac1df16f2
Roshan Kandel <rkandel@healthgrades.com>
2017-06-12 16:41:14 -0600

    server side render react node js

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import config from './config';
+import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+
+const server = express();
+
+
+server.use(sassMiddleware({
+  src:path.join(__dirname,'sass'),
+  dest:path.join(__dirname,'public')
+}));
+server.set('view engine', 'ejs');
+
+import serverRender from './serverRender';
+
+server.get('/', (req, res) => {
+  serverRender()
+    .then(({initialMarkup,initialData}) => {
+      res.render('index', {
+        initialMarkup,
+        initialData
+      });
+    })
+    .catch(console.error);
+});
+
+server.use('/api',apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port,config.host, ()=>{
+  console.info('Express is listening on port ', config.port);
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname+'/public',
-        filename:'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test:/\.js$/,
-                loader:'babel-loader'  //Run babel-loader in every .js file this transform non standard js like jsx es2015
-            }
-        ]
-    }
-}
+  entry: './src/index.js',
+  output: {
+    path: __dirname+'/public',
+    filename:'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test:/\.js$/,
+        loader:'babel-loader'  //Run babel-loader in every .js file this transform non standard js like jsx es2015
+      },
+      {
+        test:/\.json$/,
+        loader:'json-loader'  //Run babel-loader in every .js file this transform non standard js like jsx es2015
+      }
+    ]
+  }
+};
